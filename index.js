c73eb416f15671ccbc16f34e123effd06b0f2349
Hsiaoming Yang <lepture@me.com>
2012-12-21 23:13:42 +0800

    make logging instance

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var color = require('./lib/color');
 exports = module.exports = color;
 exports.program = require('./lib/program');
-exports.logging = require('./lib/logging');
+var logging = require('./lib/logging');
+exports.logging = logging;
+exports.Logging = logging.Logging;
