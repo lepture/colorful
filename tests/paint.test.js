577a06e5f9b3a11a420676ba9fef65b904d5a396
Hsiaoming Yang <lepture@me.com>
2013-03-18 17:20:06 +0800

    add tests for gray

--- tests/paint.test.js
+++ tests/paint.test.js
@@ -4,7 +4,16 @@ var styles = [
   'bold', 'faint', 'italic', 'underline', 'blink', 'overline',
   'inverse', 'conceal', 'strike'
 ];
+var colors = [
+  'black', 'red', 'green', 'yellow', 'blue',
+  'magenta', 'cyan', 'white', 'grey', 'gray'
+];
+
 
 styles.forEach(function(key) {
+  console.log(paint(key)[key].color);
+});
+
+colors.forEach(function(key) {
   console.log(paint(key)[key].color);
 });
--- tests/string.test.js
+++ tests/string.test.js
@@ -4,7 +4,17 @@ var styles = [
   'bold', 'faint', 'italic', 'underline', 'blink', 'overline',
   'inverse', 'conceal', 'strike'
 ];
+var colors = [
+  'black', 'red', 'green', 'yellow', 'blue',
+  'magenta', 'cyan', 'white', 'grey', 'gray'
+];
+
 
 styles.forEach(function(key) {
+  console.log(key.to[key].color);
+});
+
+
+colors.forEach(function(key) {
   console.log(key.to[key].color);
 });
